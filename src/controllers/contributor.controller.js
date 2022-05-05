b72a62e07582d8695ed8a7928b626425be1e5ffb
TijanAyo <ayomidetijani6@gmail.com>
2022-05-05 23:32:58 +0100

    contributor route

---
+++ src/controllers/contributor.controller.js
@@ -0,0 +1,13 @@
+const contribute = (req, res) => {
+    return res.send('Contributor: Posting')
+    // Posting to the studentArchive DB
+    // course name
+    //description - optional
+    // file - .pdf, .docx e.t.c
+
+    //@desc: Contributor would be able to make a post if authorized
+}
+
+module.exports = {
+    contribute
+}
--- src/routes/contributor.routes.js
+++ src/routes/contributor.routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const { contribute } = require('../controllers/contributor.controller')
 
 
-//  @desc: Contributor Route
 //  @desc: Authorize user to access /contributor endpoint
 router.get('/contributor', (req, res)=>{
     return res.send('Student Archive:Contributor')
 })
+
+//  @desc: Posting to the StudentArchive DB
+router.post('/contributor', contribute)
 
 module.exports = router
