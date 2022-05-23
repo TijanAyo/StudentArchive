89af345bf9d4991b942824bf141041b19ca04020
TijanAyo <ayomidetijani6@gmail.com>
2022-05-23 18:02:40 +0100

    changed endpoint and template name

--- src/routes/contributor.routes.js
+++ src/routes/contributor.routes.js
@@ -3,10 +3,9 @@ const router = express.Router()
 const { contribute, download, upload} = require('../controllers/contributor.controller')
 const { Protect } = require('../middleware/auth.middleware')
 
-
 //  @desc: Authorize user to access /contributor endpoint
-router.get('/contributor', Protect, async (req, res)=>{
-    return res.render('../views/contributor/contributor.index.ejs')
+router.get('/dashboard', Protect, async (_, res)=>{
+    return res.render('../views/contributor/dashboard.ejs')
     /* return res.json('Student Archive:Contributor') */
 })
 
